dd4e6f536181ce8aecb8a014f3c86627dcf9bf22
Isseki WATABE <oneseki743@gmail.com>
2023-02-20 22:37:13 +0900

    create ABC088B

---
+++ src/files/ABC088B_Card Game for Two.ts
@@ -0,0 +1,109 @@
+import { createInterface } from 'readline';
+import * as fs from 'fs';
+
+let inputs = '';
+let inputArray: string[];
+let currentIndex = 0;
+
+let outputBuffer = '';
+
+function next() {
+  return inputArray[currentIndex++];
+}
+function nextNum() {
+  return +next();
+}
+function nextBigInt() {
+  return BigInt(next());
+}
+function nexts(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = next();
+  return arr;
+}
+function nextNums(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = nextNum();
+  return arr;
+}
+function nextBigInts(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = nextBigInt();
+  return arr;
+}
+
+function print(out: string | number | bigint): void;
+function print<T>(out: Array<T>, separator: string): void;
+function print<T>(out: string | number | bigint | Array<T>, separator?: string) {
+  if (Array.isArray(out)) {
+    outputBuffer += out.join(separator);
+  } else {
+    outputBuffer += out;
+  }
+}
+
+function println(out: string | number | bigint): void;
+function println<T>(out: Array<T>, separator: string): void;
+function println<T>(out: string | number | bigint | Array<T>, separator?: string) {
+  if (Array.isArray(out)) {
+    print(out, separator || '');
+  } else {
+    print(out);
+  }
+  print('\n');
+}
+
+function flush() {
+  console.log(outputBuffer);
+}
+
+// デバッグ環境がWindowsであれば条件分岐する
+if (process.env.OS == 'Windows_NT') {
+  const stream = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  stream.on('line', (line) => {
+    inputs += line;
+    inputs += '\n';
+  });
+  stream.on('close', () => {
+    inputArray = inputs.split(/\s/);
+    main();
+    flush();
+  });
+} else {
+  inputs = fs.readFileSync('/dev/stdin', 'utf8');
+  inputArray = inputs.split(/\s/);
+  main();
+  flush();
+}
+
+function sortAsc(a: number, b: number) {
+  return a - b;
+}
+
+function sortDesc(a: number, b: number) {
+  return b - a;
+}
+
+function main() {
+  // ここに処理を記述していく。
+  let [N] = nextNums(1);
+  let cards = nextNums(N);
+
+  let aSum = 0;
+  let bSum = 0;
+
+  let result = 0;
+  const cardsSorted = cards.sort(sortDesc);
+  for (let i = 0; i < N; ++i) {
+    if (i % 2 === 1) {
+      bSum += cardsSorted[i];
+    } else {
+      aSum += cardsSorted[i];
+    }
+  }
+  result = aSum - bSum;
+  println(`${result}`);
+}
